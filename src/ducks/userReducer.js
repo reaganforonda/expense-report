d014b39ddedaf0b99c382229bd5e655ae40560ce
reaganforonda <foronda.reagan@gmail.com>
2018-09-04 16:47:28 -0600

    add loaduser

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -2,9 +2,24 @@ const INITIAL_STATE = {
     user: {}
 }
 
+const LOAD_USER = "LOAD_USER";
+
+export function loadUser(user){
+
+    return {
+        type: LOAD_USER,
+        payload: user
+    }
+}
+
+
 
 export default function userReducer(state=INITIAL_STATE, action ) {
     switch(action.type) {
+
+        case LOAD_USER:
+            return Object.assign({}, state, {user: action.payload})
+
         default:
             return state;
     }
