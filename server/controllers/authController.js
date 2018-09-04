d2d7044bec01902baa62bb32e98333fcac84b728
reaganforonda <foronda.reagan@gmail.com>
2018-09-04 12:24:50 -0600

    work on register

---
+++ server/controllers/authController.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+    register : async (req, res, next) => {
+        const db = req.app.get('db');
+        const {
+            firstName,
+            lastName,
+            email,
+            pw,
+            confirmPW
+        } = req.body
+
+        if(pw !== confirmPW) {
+            res.sendStatus(400);
+        };
+
+        if(pw.length > 20 && confirmPW.length > 20 ){
+            res.sendStatus(400);
+        };
+
+        await db.CHECK_EMAIL([email]).then((users) => {
+            if(users.length !== 0){
+                if(users[0].email === email) {
+                    res.status(400).send('Please Login')
+                }
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(confirmPW, salt);
+
+                db.CREATE_USER([email, hash, firstName, lastName, 1]).then((user) => {
+                    res.status(200).send('User Created');
+                }).catch((err) => {
+                    console.log(`Server error while attempting to create user: ${err}`);
+                    res.sendStatus(500);
+                })
+            }
+        }).catch((err) => {
+            console.log(`Server error while attempting to search emails: ${err}`);
+            res.sendStatus(500);
+        })
+    },
+    
+    login: (req, res, next) => {
+        const db = req.app.get('db');
+    },
+
+    logout: (req, res, next) => {
+        const db = req.app.get('db');
+    },
+
+    register: (req, res, next) => {
+        const db = req.app.get('db');
+    },
+
+    validate: (req, res, next) => {
+        const db = req.app.get('db');
+    }
+}
