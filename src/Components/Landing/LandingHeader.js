c29002821fec5c12ed5fc2bf61ea5010aa5454b7
reaganforonda <foronda.reagan@gmail.com>
2018-09-03 23:20:59 -0600

    add placeholders

--- src/Components/Landing/LandingHeader.js
+++ src/Components/Landing/LandingHeader.js
@@ -4,7 +4,18 @@ import React from 'react';
 export default function LandingHeader(props) {
     return (
         <header className='landing-header'>
-            Landing Header
+            <div className='landing-header-logo'>
+                Expenster Logo Placeholder
+            </div>
+            <menu className='landing-header-menu'>
+                <div>Link</div>
+                <div>Link</div>
+                <div>Link</div>
+            </menu>
+            <div className='lanading-header-btns'>
+                <button>Login</button>
+                <button>Sign Up</button>
+            </div>
         </header>
     )
 }
