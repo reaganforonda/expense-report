97933bd8cee01dba52d31f4fb3a3f2eadc523271
reaganforonda <foronda.reagan@gmail.com>
2018-09-03 22:41:04 -0600

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './assets/styles/main.css';
 import App from './App';
 import { unregister } from "./registerServiceWorker";
+import {HashRouter} from 'react-router-dom';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 // registerServiceWorker();
 unregister();
