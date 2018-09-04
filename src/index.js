786ebbf9d55e9464456cb6e1c6777e3c96d889ed
reaganforonda <foronda.reagan@gmail.com>
2018-09-03 22:36:31 -0600

    add main.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/styles/main.css';
 import App from './App';
 import { unregister } from "./registerServiceWorker";
 // import registerServiceWorker from './registerServiceWorker';
