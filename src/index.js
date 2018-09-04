b679867c2e68ac2d78cd38ef42e20f2a583aa9e3
reaganforonda <foronda.reagan@gmail.com>
2018-09-04 14:47:54 -0600

    add provider and connect store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/main.css';
 import App from './App';
+import {Provider} from 'react-redux';
 import { unregister } from "./registerServiceWorker";
 import {HashRouter} from 'react-router-dom';
+import store from './store';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><HashRouter><App /></HashRouter></Provider>, document.getElementById('root'));
 // registerServiceWorker();
 unregister();
