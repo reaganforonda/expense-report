20e3abb222f12d0f968663dcbc083876aa71e66e
reaganforonda <foronda.reagan@gmail.com>
2018-09-04 11:30:33 -0600

    update schema and seed

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -1,5 +1,17 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS acct_type;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS company;
+
+CREATE TABLE company (
+    company_id SERIAL PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE departments(
+    dept_id SERIAL PRIMARY KEY,
+    company_id INTEGER REFERENCES company(company_id)
+);
 
 CREATE TABLE acct_type(
     acct_type_id SERIAL PRIMARY KEY,
@@ -8,10 +20,11 @@ CREATE TABLE acct_type(
 
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
-    user_email VARCHAR(45),
-    user_first_name VARCHAR(45),
-    user_last_name VARCHAR(45),
-    user_title VARCHAR(45),
-    user_department VARCHAR(45),
+    email VARCHAR(45),
+    first_name VARCHAR(45),
+    last_name VARCHAR(45),
+    title VARCHAR(45),
+    department VARCHAR(45),
+    company INTEGER REFERENCES company(company_id),
     acct_type INTEGER REFERENCES acct_type(acct_type_id)
 );
--- db/SEED.sql
+++ db/SEED.sql
@@ -7,3 +7,8 @@ INSERT INTO acct_type
 (description)
 VALUES
 ('Employee');
+
+INSERT INTO acct_type
+(description)
+VALUES
+('Independent');
