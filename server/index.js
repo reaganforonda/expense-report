bfc80f4f7415508b4e23cc5b41904de5b6ca84fe
reaganforonda <foronda.reagan@gmail.com>
2018-09-05 14:22:08 -0600

    fix endpoint

--- server/index.js
+++ server/index.js
@@ -47,7 +47,8 @@ app.post('/api/auth/register', authController.register);
 app.post('/api/company', companyController.createNewCompany);
 app.get('/api/company/:userID', companyController.getCompany);
 app.put('/api/company', companyController.editCompany);
-app.get('/api/departments', companyController.getDepartments)
+app.get('/api/departments', companyController.getDepartments);
+app.post('/api/department', companyController.addDepartment)
 
 app.listen(SERVER_PORT, () => {
   console.log(`Creeping on Port: ${SERVER_PORT}`);
