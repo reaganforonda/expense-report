52964b188b0cd013b0466c68ac6efd49c7e08091
reaganforonda <foronda.reagan@gmail.com>
2018-09-04 21:23:12 -0600

    create company redeucer

---
+++ src/ducks/companyReducer.js
@@ -0,0 +1,25 @@
+const INITIAL_STATE = {
+    company: {}
+}
+
+const LOAD_COMPANY = "LOAD_COMPANY";
+
+export function loadCompany(company){
+    return {
+        type: LOAD_COMPANY,
+        payload: company
+    }
+}
+
+
+
+export default function companyReducer(state=INITIAL_STATE, action ) {
+    switch(action.type) {
+
+        case LOAD_COMPANY:
+            return Object.assign({}, state, {company: action.payload})
+
+        default:
+            return state;
+    }
+}
