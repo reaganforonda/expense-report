7dcc3fde7a583b8a279fb144c5eb6a3f73fa170c
reaganforonda <foronda.reagan@gmail.com>
2018-09-04 21:30:44 -0600

    added get company

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -4,6 +4,18 @@ module.exports = {
     },
 
     editCompany: (req, res) => {
-        
+
+    },
+
+    getCompany: (req, res) => {
+        const db = req.app.get('db');
+        const {userID} = req.params;
+
+        db.GET_COMPANY([userID]).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            console.log(`Server error while attempting to retrieving company: ${err}`);
+            res.sendStatus(500);
+        })
     }
 }
