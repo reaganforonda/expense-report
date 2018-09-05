f7abb190c95be1548da90eac764f304e3f7199f2
reaganforonda <foronda.reagan@gmail.com>
2018-09-05 00:51:23 -0600

    createcompany

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,6 +1,19 @@
 module.exports = {
     createNewCompany: (req, res) => {
+        const db = req.app.get('db');
+        const {
+            name, address, city,
+            state, phone, admin, zipcode
+        } = req.body;
 
+        console.log(req.body);
+
+        db.CREATE_COMPANY([name, address, city, state, zipcode, phone, admin]).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            console.log(`Server error while attempting to create new company: ${err}`);
+            res.sendStatus(500);
+        })
     },
 
     editCompany: (req, res) => {
