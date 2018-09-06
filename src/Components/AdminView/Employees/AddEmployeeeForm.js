24ff79b91777adccbc5301741034c5e6dfaac515
reaganforonda <foronda.reagan@gmail.com>
2018-09-06 13:44:47 -0600

    work on form

--- src/Components/AdminView/Employees/AddEmployeeeForm.js
+++ src/Components/AdminView/Employees/AddEmployeeeForm.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {withRouter, Switch, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as util from '../../../utilities/generalUtilities';
-
+import DepartmentDropdown from '../../DropdownMenus/DepartmentDropdown';
+import {loadEmployees, loadDepartments} from '../../../ducks/companyReducer';
+import axios from 'axios';
+import Loading from '../../Loading/Loading';
 
 export class AddEmployeeForm extends React.Component {
     constructor(props) {
         super(props);
 
         this.state={
+            userID: this.props.user.user_id,
+            companyID: this.props.company.company_id,
             firstName: '',
             lastName: '',
             department: '',
@@ -18,14 +23,51 @@ export class AddEmployeeForm extends React.Component {
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    componentDidMount(){
+        this.props.loadDepartments(this.props.company.company_id, this.props.user.user_id)
     }
 
     handleInputChange(e) {
         this.setState({[e.target.name] : e.target.value});
     }
 
+    handleSelect(e) {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let employee = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            department: this.state.department,
+            title: this.state.title,
+            workPhone: this.state.workPhone,
+            email: this.state.email
+        }
+
+        console.log(employee);
+    }
+
+    resetForm() {
+        this.setState({
+            firstName: '',
+            lastName: '',
+            department: '',
+            title: '',
+            workPhone: '',
+            email: ''
+        })
+    }
+
     render(){
+        let disabled = (this.state.firstName.length < 1) && (this.state.lastName.length < 1) && (this.state.department === '') && (this.state.title.length < 1) && (this.state.workPhone.length < 1) && (!util.validEmail(this.state.email))
         return (
+            this.props.departmentLoading ? <Loading/> : (
             <div className='employee-form'>
                 <form className='form'>
                     <div className='form-row'>
@@ -34,10 +76,10 @@ export class AddEmployeeForm extends React.Component {
                     </div>
                     <div className='form-row'>
                         <input type='text' name='lastName' value={this.state.lastName} onChange={(e)=>this.handleInputChange(e)} 
-                            placeholder='First Name'/>
+                            placeholder='Last Name'/>
                     </div>
                     <div className='form-row'>
-                        Department
+                        <DepartmentDropdown departments={this.props.departments} handleSelect={this.handleSelect}/>
                     </div>
                     <div className='form-row'>
                         <input type='text' name='title' value={this.state.title} onChange={(e)=>this.handleInputChange(e)} 
@@ -52,10 +94,10 @@ export class AddEmployeeForm extends React.Component {
                             placeholder='Email'/>
                     </div>
                     <div className='form-row'>
-                        <button>Add Employee</button>
+                        <button disabled={disabled} onClick={(e)=>this.handleSubmit(e)}>Add Employee</button>
                     </div>
                 </form>
-            </div>
+            </div>)
         )
     }
 }
@@ -64,8 +106,10 @@ export class AddEmployeeForm extends React.Component {
 function mapStateToProps(state) {
     return {
         user: state.userReducer.user,
-        company: state.companyReducer.company
+        company: state.companyReducer.company,
+        departments : state.companyReducer.departments,
+        departmentLoading: state.companyReducer.departmentLoading
     }
 }
 
-export default connect(mapStateToProps, {})(withRouter(AddEmployeeForm))
+export default connect(mapStateToProps, {loadEmployees, loadDepartments})(withRouter(AddEmployeeForm))
--- src/Components/DropdownMenus/DepartmentDropdown.js
+++ src/Components/DropdownMenus/DepartmentDropdown.js
@@ -4,12 +4,12 @@ export default function DepartmentDropdown(props) {
 
     let depts = props.departments.map((department, index) => {
         return (
-            <option value={department.name} key={department.department_id + index}>{department.name}</option>
+            <option value={department.name} key={~~department.department_id + index}>{department.name}</option>
         )
     })
 
     return (
-        <select onChange={(e)=>props.selectDepartment(e)} name='department' className='dept-dropdown'>
+        <select onChange={(e)=>props.handleSelect(e)} name='department' className='dept-dropdown'>
             <option value='' disabled selected>Department</option>
             {depts}
         </select>
