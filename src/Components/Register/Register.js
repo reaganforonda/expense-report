77370df86de63b813f101c75398f2fe8da3ea74e
reaganforonda <foronda.reagan@gmail.com>
2018-09-06 17:08:12 -0600

    button now stays disabled until account type is selected

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -73,7 +73,7 @@ export class Register extends React.Component {
     render() {
         const validPW = (this.state.pw === this.state.confirmPW) && (this.state.confirmPW.length <=25 && this.state.pw.length <=25);
         const validEmail = util.validEmail(this.state.email) && this.state.email.length > 0;
-        const submitDisabled = validPW && validEmail && this.state.pw.length > 0 && this.state.confirmPW.length > 0;
+        const submitDisabled = validPW && validEmail && this.state.pw.length > 0 && this.state.confirmPW.length > 0 && (this.state.acct_type === 1 || this.state.acct_type ===2)
         
         return (
             <div className='register'>
