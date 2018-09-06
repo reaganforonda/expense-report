8a3899e2dac16858220507b09ec46e7adb4017b3
reaganforonda <foronda.reagan@gmail.com>
2018-09-06 14:37:09 -0600

    change schema

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -1,59 +1,76 @@
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS companies;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS acct_type;
+-- DROP TABLE IF EXISTS employees;
+-- DROP TABLE IF EXISTS roles;
+-- DROP TABLE IF EXISTS departments;
+-- DROP TABLE IF EXISTS companies;
+-- DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS acct_type;
 
 
-CREATE TABLE acct_type(
-    acct_type_id SERIAL PRIMARY KEY,
+-- CREATE TABLE acct_type(
+--     acct_type_id SERIAL PRIMARY KEY,
+--     description VARCHAR(45)
+-- );
+
+-- CREATE TABLE users(
+--     user_id SERIAL PRIMARY KEY,
+--     email VARCHAR(45),
+--     pw TEXT,
+--     first_name VARCHAR(45),
+--     last_name VARCHAR(45),
+--     title VARCHAR(45),
+--     department VARCHAR(45),
+--     acct_type INTEGER REFERENCES acct_type(acct_type_id)
+-- );
+
+-- CREATE TABLE companies (
+--     company_id SERIAL PRIMARY KEY,
+--     name VARCHAR(45),
+--     address VARCHAR(45),
+--     city VARCHAR(45),
+--     state VARCHAR(45),
+--     zipcode VARCHAR(45),
+--     phone VARCHAR(45),
+--     admin INTEGER REFERENCES users(user_id)
+-- );
+
+-- CREATE TABLE departments(
+--     dept_id SERIAL PRIMARY KEY,
+--     name VARCHAR(45),
+--     company_id INTEGER REFERENCES companies(company_id)
+-- );
+
+-- CREATE TABLE roles (
+--     role_id SERIAL PRIMARY KEY,
+--     tile VARCHAR(45),
+--     description VARCHAR(45),
+--     rights JSONB
+-- );
+
+-- CREATE TABLE employees (
+--     employee_id SERIAL PRIMARY KEY,
+--     company INTEGER REFERENCES companies(company_id);
+--     first_name VARCHAR(45),
+--     last_name VARCHAR(45),
+--     department INTEGER REFERENCES departments(dept_id),
+--     title VARCHAR(45),
+--     work_phone VARCHAR(20),
+--     email VARCHAR(20),
+--     rights JSONB
+-- );
+
+DROP TABLE IF EXISTS enterprise_users;
+DROP TABLE IF EXISTS account_types;
+
+CREATE TABLE account_types (
+    acct_id SERIAL PRIMARY KEY,
     description VARCHAR(45)
 );
 
-CREATE TABLE users(
-    user_id SERIAL PRIMARY KEY,
+CREATE TABLE enterprise_users(
+    acct_id SERIAL PRIMARY KEY,
+    account_type INTEGER REFERENCES account_types(acct_id),
     email VARCHAR(45),
     pw TEXT,
     first_name VARCHAR(45),
-    last_name VARCHAR(45),
-    title VARCHAR(45),
-    department VARCHAR(45),
-    acct_type INTEGER REFERENCES acct_type(acct_type_id)
-);
-
-CREATE TABLE companies (
-    company_id SERIAL PRIMARY KEY,
-    name VARCHAR(45),
-    address VARCHAR(45),
-    city VARCHAR(45),
-    state VARCHAR(45),
-    zipcode VARCHAR(45),
-    phone VARCHAR(45),
-    admin INTEGER REFERENCES users(user_id)
-);
-
-CREATE TABLE departments(
-    dept_id SERIAL PRIMARY KEY,
-    name VARCHAR(45),
-    company_id INTEGER REFERENCES companies(company_id)
-);
-
-CREATE TABLE roles (
-    role_id SERIAL PRIMARY KEY,
-    tile VARCHAR(45),
-    description VARCHAR(45),
-    rights JSONB
-);
-
-CREATE TABLE employees (
-    employee_id SERIAL PRIMARY KEY,
-    company INTEGER REFERENCES companies(company_id);
-    first_name VARCHAR(45),
-    last_name VARCHAR(45),
-    department INTEGER REFERENCES departments(dept_id),
-    title VARCHAR(45),
-    work_phone VARCHAR(20),
-    email VARCHAR(20),
-    rights JSONB
+    last_name VARCHAR(45)
 );
--- db/SEED.sql
+++ db/SEED.sql
@@ -1,19 +1,29 @@
-INSERT INTO acct_type
-(description)
-VALUES
-('Admin');
+-- INSERT INTO acct_type
+-- (description)
+-- VALUES
+-- ('Admin');
 
-INSERT INTO acct_type
-(description)
-VALUES
-('Manager');
+-- INSERT INTO acct_type
+-- (description)
+-- VALUES
+-- ('Manager');
 
-INSERT INTO acct_type
-(description)
-VALUES
-('Employee');
+-- INSERT INTO acct_type
+-- (description)
+-- VALUES
+-- ('Employee');
 
-INSERT INTO acct_type
+-- INSERT INTO acct_type
+-- (description)
+-- VALUES
+-- ('Independent');
+
+INSERT INTO account_types
 (description)
 VALUES
-('Independent');
+('Enterprise');
+
+INSERT INTO account_types
+(description)
+VALUES
+('Individual');
