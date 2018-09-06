0aea80de8b787e1224ad6acf5346265cf108375a
reaganforonda <foronda.reagan@gmail.com>
2018-09-06 16:49:05 -0600

    moved to if statement to check if exists

--- src/Components/DropdownMenus/DepartmentDropdown.js
+++ src/Components/DropdownMenus/DepartmentDropdown.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function DepartmentDropdown(props) {
 
-    let depts = props.departments.map((department, index) => {
-        return (
-            <option value={department.dept_id} key={~~department.dept_id + index}>{department.name}</option>
-        )
-    })
-
+    let depts = []
+    if(props.department) {
+        depts = props.departments.map((department, index) => {
+            return (
+                <option value={department.dept_id} key={~~department.dept_id + index}>{department.name}</option>
+            )
+        })
+    }
+    
     return (
         <select onChange={(e)=>props.handleSelect(e)} name='department' className='dept-dropdown'>
             <option value='' disabled selected>Department</option>
