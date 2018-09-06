8eda2f62c094cbab6c9c6c32a1b7dc11006f6f1e
reaganforonda <foronda.reagan@gmail.com>
2018-09-06 13:03:07 -0600

    created dept dropdown

---
+++ src/Components/DropdownMenus/DepartmentDropdown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function DepartmentDropdown(props) {
+
+    let depts = props.departments.map((department, index) => {
+        return (
+            <option value={department.name} key={department.department_id + index}>{department.name}</option>
+        )
+    })
+
+    return (
+        <select onChange={(e)=>props.selectDepartment(e)} name='department' className='dept-dropdown'>
+            <option value='' disabled selected>Department</option>
+            {depts}
+        </select>
+    )
+}
