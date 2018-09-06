d086085628ece469a9ccff91ca6a1298f0307243
reaganforonda <foronda.reagan@gmail.com>
2018-09-05 18:41:50 -0600

    styling

--- src/Components/DashboardView/DashboardHeader.js
+++ src/Components/DashboardView/DashboardHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function DashboardHeader(props){
     return (
         <header className='dashboard-header'>
-            header
+            
         </header>
     )
 }
