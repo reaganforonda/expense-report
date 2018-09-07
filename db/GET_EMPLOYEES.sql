07a90eef7c86391652a21318b55cb04feb832a5c
reaganforonda <foronda.reagan@gmail.com>
2018-09-06 20:20:47 -0600

    fix bug

--- db/GET_EMPLOYEES.sql
+++ db/GET_EMPLOYEES.sql
@@ -1,3 +1,5 @@
 SELECT *
 FROM employees
-WHERE company = $1
+JOIN departments on departments.dept_id = employees.department
+WHERE ($1 IS NULL OR employees.company = $1)
+AND ($2 IS NULL OR employees.employee_id = $2)
