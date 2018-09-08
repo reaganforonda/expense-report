dc2b96c0e044a58640aaba47b13ea334915b20c8
reaganforonda <foronda.reagan@gmail.com>
2018-09-08 14:35:39 -0600

    work on add new report

--- server/controllers/expenseReportController.js
+++ server/controllers/expenseReportController.js
@@ -1,6 +1,25 @@
 module.exports = {
     createNewReport : (req, res) => {
+        const db = req.app.get('db');
+        const {
+            employee,
+            reportDate,
+            description,
+            user
+        } = req.body;
 
+        console.log(req.body);
+
+        // if(user.rights.Expense) {
+        //     db.CREATE_NEW_EXP_REPORT([employee, reportDate, description]).then((result) => {
+        //         res.status(200).send(result);
+        //     }).catch((err)=> {
+        //         consol.log(`Server error while attempting to create new report: ${err}`);
+        //         re.sendStatus(500);
+        //     })
+        // } else {
+        //     res.sendStatus(401);
+        // }
     },
 
     editExpenseReport : (req, res) => {
@@ -16,6 +35,6 @@ module.exports = {
     },
 
     getExpenseReports : (req, res) => {
-        
+
     }
 }
