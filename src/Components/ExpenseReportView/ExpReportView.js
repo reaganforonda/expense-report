a1fed7419276cdaadea5db7b10b8edab3448dd97
reaganforonda <foronda.reagan@gmail.com>
2018-09-08 15:51:07 -0600

    work on expense reports

--- src/Components/ExpenseReportView/ExpReportView.js
+++ src/Components/ExpenseReportView/ExpReportView.js
@@ -5,6 +5,7 @@ import Loading from '../Loading/Loading';
 import ExpReportReports from './ExpReportReports';
 import ExpReportExpenses from './ExpReportExpenses';
 import ExpReportViewHeader from './ExpReportViewHeader';
+import {loadExpenseReports} from '../../ducks/expenseReducer';
 
 export class ExpReportView extends React.Component{
     constructor(props) {
@@ -13,15 +14,24 @@ export class ExpReportView extends React.Component{
         this.state={}
     }
 
+    componentDidMount(){
+        this.props.loadExpenseReports(this.props.user.employee_id);
+    }
+
     render(){
         return (
             <div className='exp-report-view'>
                 <ExpReportViewHeader/>
+                
                 <main>
-                    <Switch>
-                        <Route exact path='/dashboard/expense/reports' component={ExpReportReports}/>
-                        <Route exact path='/dashboard/expense/expenses' component={ExpReportExpenses}/>
-                    </Switch>
+                    {
+                        this.props.reportLoading ? <Loading/> : (
+                        <Switch>
+                            <Route exact path='/dashboard/expense/reports' component={ExpReportReports}/>
+                            <Route exact path='/dashboard/expense/expenses' component={ExpReportExpenses}/>
+                        </Switch>
+                        )
+                    }
                 </main>
             </div>
         )
@@ -30,8 +40,10 @@ export class ExpReportView extends React.Component{
 
 function mapStateToProps(state) {
     return {
-        user: state.userReducer.user
+        user: state.userReducer.user,
+        reportLoading: state.expenseReducer.reportLoading,
+        expensesLoading: state.expenseReducer.expensesLoading,
     }
 }
 
-export default connect(mapStateToProps, {})(withRouter(ExpReportView));
+export default connect(mapStateToProps, {loadExpenseReports})(withRouter(ExpReportView));
--- src/Components/ExpenseReportView/ReportFormModal.js
+++ src/Components/ExpenseReportView/ReportFormModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import {connect} from 'react-redux';
 
 export default class ReportFormModal extends React.Component{
     constructor(props) {
