1f64dcb8d1978e4f52aef9dae326312786f192c8
reaganforonda <foronda.reagan@gmail.com>
2018-09-07 20:05:37 -0600

    added controller functions

---
+++ server/controllers/userController.js
@@ -0,0 +1,42 @@
+module.exports = {
+    createUser: (req, res) => {
+        const db = req.app.get('db');
+        const {
+            account_type,
+            email,
+            firstName,
+            lastName,
+            temppassword,
+            rights,
+            adminUser
+        } = req.body;
+
+        console.log(req.body);
+
+        // Expiration Date for Temp Password
+        const expireDate = new Date();
+        expireDate.setDate(expireDate.getDate() + 1);
+
+        if(adminUser.rights.Admin){
+            db.CREATE_NEW_USER([account_type, email, temppassword, expireDate, firstName, lastName, rights]).then((result) => {
+                console.log(result[0]);
+                res.status(500).send(result[0]);
+            }).catch((err) => {
+                console.log(`Server error while attempting to create new user: ${err}`)
+                res.sendStatus(500);
+            })
+        }
+    },
+
+    resetPW: (req, res) => {
+
+    },
+
+    deactiveUser: (req, res) => {
+
+    },
+
+    reactiveUser: (req, res) => {
+
+    }
+}
