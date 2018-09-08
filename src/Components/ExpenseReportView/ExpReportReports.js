28acefacdf564bd76b64707fc4c5cbad2e2e21c1
reaganforonda <foronda.reagan@gmail.com>
2018-09-08 14:35:49 -0600

    work on report and modal

--- src/Components/ExpenseReportView/ExpReportReports.js
+++ src/Components/ExpenseReportView/ExpReportReports.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import {withRouter, Switch, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
+import ReportFormModal from './ReportFormModal';
 
 export class ExpReportReports extends React.Component{
     constructor(props) {
         super(props);
 
-        this.state={}
+        this.state={
+            displayReportForm: false
+        }
+
+        this.handleDisplayReportForm = this.handleDisplayReportForm.bind(this);
+    }
+
+    handleDisplayReportForm(){
+        if(this.state.displayReportForm === false){
+            this.setState({displayReportForm: true})
+        } else {
+            this.setState({displayReportForm: false})
+        }
     }
 
     render(){
         return (
             <div className='exp-main'>
                 <header className='exp-report-header'>
-                    <button>New Expense Report</button>
+                    {
+                        this.state.displayReportForm ? null : <button onClick={()=>this.handleDisplayReportForm()}type='button'>New Expense Report</button>
+                    }
+                    {
+                        this.state.displayReportForm ? <ReportFormModal user={this.props.user} closeForm={this.handleDisplayReportForm}/> : null
+                    }
                 </header>
                 <main >
 
---
+++ src/Components/ExpenseReportView/ReportFormModal.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class ReportFormModal extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={
+            date: '',
+            description: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    handleSubmitForm(e) {
+        e.preventDefault();
+
+        let report = {
+            user: this.props.user,
+            date: this.state.date,
+            description: this.state.description
+        }
+
+        console.log(report);
+    }
+
+    render(){
+        const disabled = (this.state.date === '') && (this.state.description.length < 1);
+        return (
+            <main className='report-form-modal'>
+                <form className='report-form'>
+                    <div className='form-row'>
+                        <p>Employee Name</p>
+                    </div>
+                    <div className='form-row'>
+                        <p>Employee Department</p>
+                    </div>
+                    <div className='form-row'>
+                        <div>Report Date</div> <input type='date' name='date' value={this.state.date} placeholder='Report Date' onChange={(e)=>this.handleInputChange(e)}/>
+                    </div>
+                    <div className='form-row'>
+                        <div>Description</div> <input type='text' name='description' value={this.state.description} maxLength={50} placeholder='Report Description' onChange={(e)=>this.handleInputChange(e)}/>
+                    </div>
+                    <div className='form-row'>
+                        <button onClick={()=>this.props.closeForm()}type='button'>Cancel</button>
+                        <button disabled={disabled} onClick={(e)=>this.handleSubmitForm(e)}>Submit</button>
+                    </div>
+                </form>
+            </main>
+        )
+    }
+}
