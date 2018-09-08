a408f2b5a336801cca56d5aee8fb9d6b5e3abdfb
reaganforonda <foronda.reagan@gmail.com>
2018-09-08 16:07:31 -0600

    narrowed select columns

--- db/GET_EXPENSE_REPORTS.sql
+++ db/GET_EXPENSE_REPORTS.sql
@@ -1,5 +1,4 @@
-SELECT * 
-From expense_reports
-FULL OUTER JOIN expenses 
-ON expenses.report_id = expense_reports.report_id
+select expense_reports.report_id, expense_reports.report_number, expense_reports.employee, expense_reports.report_date, expense_reports.description, expense_reports.approved, expenses.expense_id, expenses.expense_date, expenses.merchant, expenses.amount, expenses.category, expenses.comment, expenses.tag, expenses.img
+from expense_reports
+full outer join expenses on expenses.report_id = expense_reports.report_id
 WHERE expense_reports.employee = $1
