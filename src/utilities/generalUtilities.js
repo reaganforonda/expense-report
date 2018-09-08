00aba1ae0e564c0e9b03e03637d51bfbd345c76a
reaganforonda <foronda.reagan@gmail.com>
2018-09-07 19:32:49 -0600

    added a function to generate a random password

--- src/utilities/generalUtilities.js
+++ src/utilities/generalUtilities.js
@@ -67,4 +67,13 @@ module.exports = {
            return false;
        }
    },
+
+   generateRandomString(length) {
+       let randomString='';
+       const characters ='abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+       for(let i =0; i <= length; i++) {
+           randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+       }
+       return randomString;
+   }
 }
