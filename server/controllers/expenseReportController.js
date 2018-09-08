04df66170b6adde17a334eb82d22eb4e078f19e1
reaganforonda <foronda.reagan@gmail.com>
2018-09-08 10:33:33 -0600

    create expense controller

---
+++ server/controllers/expenseReportController.js
@@ -0,0 +1,21 @@
+module.exports = {
+    createNewReport : (req, res) => {
+
+    },
+
+    editExpenseReport : (req, res) => {
+
+    },
+
+    deleteExpenseReport : (req, res) => {
+
+    },
+
+    approveExpenseReport: (req, res) => {
+
+    },
+
+    getExpenseReports : (req, res) => {
+        
+    }
+}
