ce20f31e4276e06b555ec2671eb942068bea2f74
reaganforonda <foronda.reagan@gmail.com>
2018-09-08 08:35:42 -0600

    added outer join to user table to get rights

--- db/GET_EMPLOYEES.sql
+++ db/GET_EMPLOYEES.sql
@@ -1,5 +1,6 @@
-SELECT *
+SELECT employees.employee_id, employees.user_id, employees.company, employees.department, employees.first_name, employees.last_name, employees.title, employees.work_phone, employees.email, departments.name, users.rights, users.lastlogin
 FROM employees
 JOIN departments on departments.dept_id = employees.department
+FULL OUTER JOIN users ON users.user_id = employees.user_id
 WHERE ($1 IS NULL OR employees.company = $1)
 AND ($2 IS NULL OR employees.employee_id = $2)
