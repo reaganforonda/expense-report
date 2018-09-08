2f4d978030a548491df64b53fc3fa262085ec909
reaganforonda <foronda.reagan@gmail.com>
2018-09-08 13:11:50 -0600

    fix typos

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS expense,
+DROP TABLE IF EXISTS expenses;
 DROP TABLE IF EXISTS expense_reports;
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS departments;
@@ -57,19 +57,19 @@ CREATE TABLE expense_reports (
     report_id SERIAL PRIMARY KEY,
     report_number VARCHAR(10),
     employee INTEGER REFERENCES employees(employee_id),
-    date DATE,
+    report_date DATE,
     description VARCHAR(50),
     approved BOOLEAN
-)
+);
 
-CREATE TABLE expense (
+CREATE TABLE expenses (
     expense_id SERIAL PRIMARY KEY,
-    report_id INTEGER REFERENCES expense_reports(report_id)
-    date Date,
+    report_id INTEGER REFERENCES expense_reports(report_id),
+    expense_date Date,
     merchant VARCHAR(45),
     amount NUMERIC,
     category VARCHAR(45),
     comment VARCHAR(100),
     tag TEXT[],
     img TEXT
-)
+);
