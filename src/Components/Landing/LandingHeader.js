b9b9db3331c47ac87594f152cf7c60bcf35ee49f
reaganforonda <foronda.reagan@gmail.com>
2018-09-10 14:36:52 -0600

    removed commented section

--- src/Components/Landing/LandingHeader.js
+++ src/Components/Landing/LandingHeader.js
@@ -7,11 +7,6 @@ export default function LandingHeader(props) {
             <div className='logo'>
                 <h1>EXPENSTER</h1>
             </div>
-            {/* <menu className='menu'>
-                <div>Link</div>
-                <div>Link</div>
-                <div>Link</div>
-            </menu> */}
             <div className='btns'>
                 <button onClick={(e)=>props.register(e)}>Sign Up</button>
                 <button onClick={(e)=>props.login(e)}>Log In</button>
