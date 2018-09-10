c419e5258dcb7f065fdb13c492f0af41e74b1fe6
reaganforonda <foronda.reagan@gmail.com>
2018-09-10 14:42:34 -0600

    edit

--- db/CHECK_EMAIL.sql
+++ db/CHECK_EMAIL.sql
@@ -1,4 +1,4 @@
-SELECT *
+SELECT users.user_id, users.account_type, users.email, users.pw, users.first_name, users.last_name, users.temppassword, users.tempexpiration, users.lastlogin, users.rights, employees.employee_id, employees.department, employees.title, employees.work_phone
 FROM users
 FULL OUTER JOIN employees on users.user_id = employees.user_id
 WHERE users.email = $1
