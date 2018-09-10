3a8f0da7ac65b2371a65be76340e9d62aa7a0014
reaganforonda <foronda.reagan@gmail.com>
2018-09-10 11:10:15 -0600

    added report_number

--- db/CREATE_NEW_EXP_REPORT.sql
+++ db/CREATE_NEW_EXP_REPORT.sql
@@ -1,4 +1,4 @@
 INSERT INTO expense_reports
-(employee, report_date, description, approved)
+(employee, report_date, description, report_number, approved)
 VALUES
-($1, $2, $3, false);
+($1, $2, $3, $4, false);
