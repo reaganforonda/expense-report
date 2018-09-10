b434298b019d28f1a217a0672d84aa74252e1c01
reaganforonda <foronda.reagan@gmail.com>
2018-09-09 19:12:40 -0600

    styling

--- src/Components/ExpenseReportView/ExpenseList.js
+++ src/Components/ExpenseReportView/ExpenseList.js
@@ -1,17 +1,20 @@
 import React from'react';
+import * as util from '../../utilities/generalUtilities'
 
 export default function ExpenseList (props) {
     
     let expenseList = props.expenses.map((expense) => {
         return (
             <div className='expense-item' key = {expense.expense_id}>
-                {expense.expense_id}
-                {expense.expense_date}
-                {expense.merchant}
-                {expense.amount}
-                {expense.category}
-                {expense.comment}
-                {expense.report_id}
+                <div>{expense.expense_id}</div>
+                <div>{util.formatDate(expense.expense_date)}</div>
+                <div>{expense.merchant}</div>
+                <div>{expense.amount}</div>
+                <div>{expense.comment}</div>
+                <div>{expense.category}</div>
+                <div>{expense.report_id}</div>
+                <div><input type='checkbox' id='expense-add-to-report'/></div>
+                <div><button type='button'>Edit</button></div>
             </div>
         )
     })
@@ -19,13 +22,17 @@ export default function ExpenseList (props) {
 
     return (
         <div className='expense-list'>
-            <div>
+            <div className='exp-list-header'>
                 <h2>Expense ID</h2>
                 <h2>Expense Date</h2>
-                <h2>Comment</h2>
                 <h2>Merchant</h2>
                 <h2>Amount</h2>
+                <h2>Comment</h2>
+                <h2>Category</h2>
                 <h2>Report ID</h2>
+                <div>Select</div>
+                <div>Edit</div>
+                
             </div>
             {expenseList}
         </div>
