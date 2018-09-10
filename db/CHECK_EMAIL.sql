201715032a90dff29fe09dd1deee646e50ae0a08
reaganforonda <foronda.reagan@gmail.com>
2018-09-10 16:41:52 -0600

    add returning

--- db/CHECK_EMAIL.sql
+++ db/CHECK_EMAIL.sql
@@ -1,4 +1,4 @@
-SELECT users.user_id, users.account_type, users.email, users.pw, users.first_name, users.last_name, users.temppassword, users.tempexpiration, users.lastlogin, users.rights, employees.employee_id, employees.department, employees.title, employees.work_phone
+SELECT users.user_id, users.account_type, users.email, users.pw, users.first_name, users.last_name, users.temppassword, users.tempexpiration, users.lastlogin, users.rights, employees.employee_id, employees.department, employees.title, employees.work_phone, employees.company
 FROM users
 FULL OUTER JOIN employees on users.user_id = employees.user_id
 WHERE users.email = $1
