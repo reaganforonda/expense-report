8ee10cd3e98a1b2de68a1b779e9ba89083bd192d
reaganforonda <foronda.reagan@gmail.com>
2018-09-09 21:12:04 -0600

    work on report dropdown

--- src/Components/DropdownMenus/ReportsDropdown.js
+++ src/Components/DropdownMenus/ReportsDropdown.js
@@ -1,2 +1,20 @@
 import React from 'react';
 
+export default function ReportsDropdown (props) {
+    let reports = []
+
+    if(props.reports) {
+        reports = props.reports.map((report, index) =>{
+            return (
+                <option value={report.report_id} key={~~report.report_id}>{report.report_id}-{report.description}</option>
+            )
+        })
+    }
+
+    return (
+        <select name='report' className='report-dropdown'>
+            <option value='' disabled selected>Report</option>
+            {reports}
+        </select>
+    )
+}
