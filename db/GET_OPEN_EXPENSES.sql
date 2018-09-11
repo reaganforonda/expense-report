68c552b9901bcbcd37a46b6bc977c89fb760558b
reaganforonda <foronda.reagan@gmail.com>
2018-09-11 15:36:10 -0600

    fix bug

--- db/GET_OPEN_EXPENSES.sql
+++ db/GET_OPEN_EXPENSES.sql
@@ -1,5 +1,5 @@
 SELECT expenses.expense_id, expenses.report_id, expenses.employee, expenses.expense_date, expenses.merchant, expenses.amount, expenses.category, expenses.comment, expenses.tag, expenses.img, expense_reports.status
 FROM EXPENSES
-FULL OUTER JOIN expense_reports ON expense_reports.report_id = expenses.report_id
+LEFT JOIN expense_reports ON expense_reports.report_id = expenses.report_id
 WHERE expenses.report_id IS NULL or expense_reports.status @> '{"Submitted": false}'
 AND ($1 IS NULL OR expenses.employee = $1)
