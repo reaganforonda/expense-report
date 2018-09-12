a35bcd3fe32671109cda16adfe0b1be30c6f0009
reaganforonda <foronda.reagan@gmail.com>
2018-09-11 20:54:06 -0600

    update query

--- db/GET_PENDING_REPORTS.sql
+++ db/GET_PENDING_REPORTS.sql
@@ -1,4 +1,6 @@
-SELECT * FROM expense_reports
-JOIN employees ON employees.employee_id = expense_reports.employee
-WHERE expense_reports.status @> '{"Submitted": true}'
-AND employees.approver = $1
+select expense_reports.report_id, expense_reports.report_number, expense_reports.report_date, expense_reports.description, expense_reports.status, expense_reports.employee, employees.first_name, employees.last_name, employees.email, employees.user_id, sum(expenses.amount) as total from expense_reports
+join employees on employees.employee_id = expense_reports.employee
+join expenses on expenses.report_id = expense_reports.report_id
+where expense_reports.status @> '{"Submitted": true}'
+and employees.approver = $1
+group by expense_reports.report_id, employees.first_name, employees.last_name, employees.email, employees.user_id
