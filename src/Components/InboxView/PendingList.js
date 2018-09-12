604976cbc44dbe0daf83a0ba8a4d9b2c80ee4a57
reaganforonda <foronda.reagan@gmail.com>
2018-09-11 22:16:59 -0600

    work on pending list

--- src/Components/InboxView/PendingList.js
+++ src/Components/InboxView/PendingList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as util from '../../utilities/generalUtilities';
-import axios form 'axios';
+import axios from 'axios';
 
 export default class PendingList extends React.Component{
     constructor(props) {
@@ -10,16 +10,48 @@ export default class PendingList extends React.Component{
             displayDetails: false,
             expenses: []
         }
+
+        this.getExpenses = this.getExpenses.bind(this);
     }
 
     getExpenses(reportID) {
-        
+        if(this.state.displayDetails===false) {
+        axios.get(`/api/expenses?reportID=${reportID}`).then((result) => {
+            console.log(result.data);
+            this.setState({expenses: result.data, displayDetails: true});
+        })} else {
+            this.setState({displayDetails: false})
+        }
+    }
+
+    closeDetail(){
+        this.setState({displayDetails: false})
     }
 
     render(){
+        let expenseList = '';
+
+        if(this.state.expenses.length > 0) {
+            expenseList = this.state.expenses.map((expense) => {
+                return (
+                    <div className='expense-list-item-pending' key={expense.expense_id}>
+                        <div>{expense.expense_id}</div>
+                        <div>{expense.expense_date}</div>
+                        <div>{expense.merchant}</div>
+                        <div>{expense.amount}</div>
+                        <div>{expense.category}</div>
+                        <div>{expense.comment}</div>
+                        <div>{expense.tag}</div>
+                        <div>{expense.img}</div>
+                    </div>
+                )
+            })
+        }
+        console.log(expenseList);
         let reports = this.props.pendingReports.map((report) => {
+            
             return (
-                <div className='pending-report-item' key={report.report_id}>
+                <div onClick={()=>this.getExpenses(report.report_id)}className='pending-report-item' key={report.report_id}>
                     <div>{report.report_number}</div>
                     <div>{report.first_name} {report.last_name}</div>
                     <div>{util.formatDate(report.report_date)}</div>
@@ -28,10 +60,14 @@ export default class PendingList extends React.Component{
                 </div>
             )
         })
+
         return (
             <div className='pending-list'>
-            {reports}
-        </div>
+                {reports}
+                
+                    {this.state.displayDetails ? <div>{expenseList}<div><button onClick={()=>this.closeDetail()}>Close</button></div></div> : null}
+                
+            </div>
         )
     }
 }
